497b9ec601a3f682b201f0ec40b51f62088ef9cb
Dmytro Furman <dmytro.furman@beetroot.se>
2022-11-20 14:36:19 +0200

    general settings | scss file structure, components, mixins, fonts, reset

--- components/Form.js
+++ components/Form.js
@@ -1,1 +1,7 @@
 import styles from '../styles/components/Form.module.scss'
+
+export default function () {
+  return <>
+    <h2>Form Element</h2>
+  </>
+}
--- pages/index.js
+++ pages/index.js
@@ -1,19 +1,17 @@
 import Head from 'next/head'
 import styles from '../styles/components/Home.module.scss'
+import Form from '../components/Form'
 
 export default function Home() {
-  return (
-    <div className={styles.container}>
-      <Head>
-        <title>Web Interface Labs</title>
-        <meta name="description" content="Simple template for my lab tasks on the subject" />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+  return (<>
+    <Head>
+      <title>Web Interface Labs</title>
+      <meta name="description" content="Simple template for my lab tasks on the subject" />
+      <link rel="icon" href="/favicon.ico" />
+    </Head>
 
-      <main className={styles.main}>
-
-      </main>
-
-    </div>
-  )
+    <main className={styles.main}>
+      <Form />
+    </main>
+  </>)
 }
